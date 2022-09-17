48e303dad0a53adbed27432fb10c9c1620ae5197
Muhammed Afsal <auxafsal@gmail.com>
2022-09-17 15:42:57 +0530

    gallery list updated

--- app/src/main/java/com/afsal/dev/filimapp/ui/HomeFragment.kt
+++ app/src/main/java/com/afsal/dev/filimapp/ui/HomeFragment.kt
@@ -2,39 +2,36 @@ package com.afsal.dev.filimapp.ui
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.get
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.afsal.dev.filimapp.R
 import com.afsal.dev.filimapp.adapters.CatagoryMoviesAdapter
 import com.afsal.dev.filimapp.adapters.LatestMoviesAdapter
 import com.afsal.dev.filimapp.databinding.FragmentHomeBinding
-import com.afsal.dev.filimapp.models.CategoriesListData
-import com.afsal.dev.filimapp.network.Resource
+import com.afsal.dev.filimapp.models.MovieItem
 import com.afsal.dev.filimapp.viewModel.MoviesViewModel
-import com.afsal.dev.typicodapp.uttil.handleApiError
 
 class HomeFragment : Fragment() {
 
-   private   var _binding: FragmentHomeBinding?=null
+    private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
     private lateinit var viewModel: MoviesViewModel
     private lateinit var latestMoviesAdapter: LatestMoviesAdapter
-    private lateinit var catagoryAdapter:CatagoryMoviesAdapter
+    private lateinit var catagoryAdapter: CatagoryMoviesAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding=FragmentHomeBinding.inflate(inflater,container,false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        viewModel= ViewModelProvider(requireActivity())[MoviesViewModel::class.java]
+        viewModel = ViewModelProvider(requireActivity())[MoviesViewModel::class.java]
 
         return binding.root
     }
@@ -42,57 +39,61 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        latestMoviesAdapter= LatestMoviesAdapter()
-        catagoryAdapter= CatagoryMoviesAdapter(){
+        latestMoviesAdapter = LatestMoviesAdapter()
 
-                  Log.d("HHH","list of data ${it.categoryName}")
-          ////////////////  navigateToMoveFragment()
+        catagoryAdapter = CatagoryMoviesAdapter() {
+
+            Log.d("HHH", "list of data ${it.categoryName}")
+            navigateToMoveFragment(it.movies)
         }
-          setView()
-         binding.moreBt1.setOnClickListener {
-             //navigateToMoveFragment()
-             }
+
+
+        setView()
+        binding.moreBt1.setOnClickListener {
+            // navigateToMoveFragment()
+        }
 
 
         viewModel.categoryList.observe(viewLifecycleOwner, Observer {
-            Log.d("TTT","Catagory result ${it}") })
+            Log.d("TTT", "Catagory result ${it}")
+        })
 
         viewModel.sortedMoviesData.observe(viewLifecycleOwner, Observer {
 
-            Log.d("GGGG","sorted result ${it.toString()}")
+            Log.d("GGGG", "sorted result ${it.toString()}")
             catagoryAdapter.differ.submitList(it)
         })
 
     }
 
-    private fun setView(){
+    private fun setView() {
         binding.topRv.apply {
-             layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-            adapter=latestMoviesAdapter
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            adapter = latestMoviesAdapter
             setHasFixedSize(true)
             setItemViewCacheSize(8)
         }
 
 
         binding.baseRv.apply {
-            layoutManager=LinearLayoutManager(context)
-            adapter=catagoryAdapter
+            layoutManager = LinearLayoutManager(context)
+            adapter = catagoryAdapter
 
             setItemViewCacheSize(10)
         }
     }
 
-    private fun navigateToMoveFragment(data :CategoriesListData){
+    private fun navigateToMoveFragment(data: List<MovieItem>) {
 
+        viewModel.selectdMovies.postValue(data)
+        //  val action= MoviesFragmentDirections.actionNavigationImagesToImageViewFragment(data)
+        findNavController().navigate(R.id.action_homeFragment_to_moviesFragment)
 
-      //   val action= MoviesFragmentDirections.actionNavigationImagesToImageViewFragment(data)
-//      findNavController().navigate(action)
-      //  findNavController().navigate()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        _binding=null
+        _binding = null
     }
 
 }
--- app/src/main/java/com/afsal/dev/filimapp/ui/MoviesFragment.kt
+++ app/src/main/java/com/afsal/dev/filimapp/ui/MoviesFragment.kt
@@ -1,22 +1,69 @@
 package com.afsal.dev.filimapp.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.afsal.dev.filimapp.R
+import com.afsal.dev.filimapp.adapters.GalleryAdapter
+import com.afsal.dev.filimapp.databinding.FragmentMoviesBinding
+import com.afsal.dev.filimapp.viewModel.MoviesViewModel
 
 
 class MoviesFragment : Fragment() {
 
 
+    private var _binding: FragmentMoviesBinding?= null
+        val binding get() = _binding !!
+    private lateinit var galleryAdapter: GalleryAdapter
+   private lateinit var viewModel: MoviesViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movies, container, false)
+
+
+        _binding=FragmentMoviesBinding.inflate(inflater,container,false)
+
+       viewModel = ViewModelProvider(requireActivity())[MoviesViewModel::class.java]
+
+     return  binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        galleryAdapter=GalleryAdapter()
+        setView()
+
+
+        viewModel.selectdMovies.observe(requireActivity(), Observer {
+            Log.d("GGG","Selected movies ${it.toString()}")
+
+            galleryAdapter.differ.submitList(it)
+        })
+    }
+
+
+    private fun setView(){
+
+        binding.moviesRv.apply {
+                layoutManager=GridLayoutManager(context,3)
+                adapter=galleryAdapter
+
+            }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        _binding=null
     }
 
 }
